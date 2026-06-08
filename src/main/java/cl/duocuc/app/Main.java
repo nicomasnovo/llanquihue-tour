bf7549bf41578b5e560a7c3b0bfff14a84ce298b
Nico Masnovo <nicomasnovo@gmail.com>
2026-06-08 17:30:27 -0400

    update main file with error validacion

--- src/main/java/cl/duocuc/app/Main.java
+++ src/main/java/cl/duocuc/app/Main.java
@@ -27,7 +27,9 @@ public class Main {
 
         Persona cliente = new Persona("1111111-1","Ana Torres", direccion1);
 
-        Empleado guia = new Empleado("22222222-2","Juan Pérez", direccion2, "Guía Turístico");;
+        Empleado guia = new Empleado("22222222-2","Juan Pérez", direccion2, "Guía Turístico");
+
+        Empleado guia2 = new Empleado("22222222","Persona Error", direccion2, "Error turistico");;
 
         Operador operador = new Operador("PatagoniaTours", "Alojamiento");
 
